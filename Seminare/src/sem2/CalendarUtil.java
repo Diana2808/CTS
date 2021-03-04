98328e4e53b669d99433e410d4b3a1d2dee91d22
Diana2808 <petrediana18@stud.ase.ro>
2021-03-04 14:14:08 +0200

    edit

--- Seminare/src/sem2/CalendarUtil.java
+++ Seminare/src/sem2/CalendarUtil.java
@@ -2,7 +2,7 @@ package sem2;
 
 public class CalendarUtil {
 
-	public String weekdDay2(int day) {
+	public String weekDay2(int day) {
 		switch(day) {
 			case 1:
 			return "Sunday";
@@ -27,7 +27,7 @@ public class CalendarUtil {
 	}
 	
 	
-	public String weekdDay(int day) {
+	public String weekDay(int day) {
 		if(day == 1)
 			return "Sunday";
 		else if (day == 2)
@@ -47,5 +47,9 @@ public class CalendarUtil {
 					
 	}
 
+	 public String weekDay3(int day){
+	        String[] weekDays = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+	        return weekDays[day-1];
+	    }
 
 }
--- Seminare/src/sem2/Main.java
+++ Seminare/src/sem2/Main.java
@@ -4,7 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		CalendarUtil cu = new CalendarUtil();
 		
-		System.out.println(cu.weekdDay2(3));
+		System.out.println(cu.weekDay(3));
+		System.out.println(cu.weekDay2(3));
+		System.out.println(cu.weekDay3(3));
+
 
 	}
 }
