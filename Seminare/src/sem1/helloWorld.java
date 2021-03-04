2ad6ee8ad6f9de5508ab042649cf694bbab32755
Diana2808 <petrediana18@stud.ase.ro>
2021-03-04 14:11:41 +0200

    Sem1 si sem2

---
+++ Seminare/src/sem1/helloWorld.java
@@ -0,0 +1,12 @@
+package sem1;
+
+public class helloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Salut World!");
+		
+		Persoana p1 = new Persoana("Chuck", "Norris");
+		
+		System.out.println(p1.toString());
+	}
+}
---
+++ Seminare/src/sem2/Main.java
@@ -0,0 +1,10 @@
+package sem2;
+
+public class Main {
+	public static void main(String[] args) {
+		CalendarUtil cu = new CalendarUtil();
+		
+		System.out.println(cu.weekdDay2(3));
+
+	}
+}
