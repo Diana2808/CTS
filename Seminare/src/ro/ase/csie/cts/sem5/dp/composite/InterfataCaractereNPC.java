d2685585b37d24b3cc771af5dbbc95d6e11c4e9d
Diana2808 <petrediana18@stud.ase.ro>
2021-04-16 10:34:20 +0300

    Interpretare composite

---
+++ Seminare/src/ro/ase/csie/cts/sem5/dp/composite/InterfataCaractereNPC.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.sem5.dp.composite;
+
+public interface InterfataCaractereNPC {
+	//toate sunt abstracte
+	public void atacaJucator(String numeJucator);
+	public void seRetrage();
+	public void seDeplaseaza();
+}
