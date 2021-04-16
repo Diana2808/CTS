8fe3554cedaf290eb63c0f288104705708323919
Diana2808 <petrediana18@stud.ase.ro>
2021-04-16 11:24:47 +0300

    Implementare FlyWeight

---
+++ Seminare/src/ro/ase/csie/cts/sem5/dp/flyweight/ContextEcran.java
@@ -0,0 +1,42 @@
+package ro.ase.csie.cts.sem5.dp.flyweight;
+
+//pentru a gestiona starea temporara
+public class ContextEcran {
+
+	int x;
+	int y;
+	String culoareTextura;
+	
+	public ContextEcran(int x, int y, String culoareTextura) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.culoareTextura = culoareTextura;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String getCuloareTextura() {
+		return culoareTextura;
+	}
+	
+	public void setCuloareTextura(String culoareTextura) {
+		this.culoareTextura = culoareTextura;
+	}
+	
+	
+}
