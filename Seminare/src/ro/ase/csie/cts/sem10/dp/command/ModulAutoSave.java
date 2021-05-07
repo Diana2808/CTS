a9f9fdc86a8b6eb4e4e9dc3a9421ee872ae9ffa6
Diana2808 <petrediana18@stud.ase.ro>
2021-05-07 17:47:37 +0300

    Command fara manager

---
+++ Seminare/src/ro/ase/csie/cts/sem10/dp/command/ModulAutoSave.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.sem10.dp.command;
+
+public class ModulAutoSave implements InterfataModulJoc {
+	@Override
+	public void executaActiune(String numeActiune) {
+		System.out.println(numeActiune);
+		
+	}
+
+}
---
+++ Seminare/src/ro/ase/csie/cts/sem10/dp/command/ModulUpdateClient.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.sem10.dp.command;
+
+public class ModulUpdateClient implements InterfataModulJoc {
+	@Override
+	public void executaActiune(String numeActiune) {
+		System.out.println("Update client joc. Detalii: " + numeActiune);
+		
+	}
+}
