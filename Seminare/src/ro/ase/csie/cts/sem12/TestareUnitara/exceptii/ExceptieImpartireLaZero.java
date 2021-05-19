8c5b0ba48f425df7b7abe35b000cf89e7a8f171d
Diana2808 <petrediana18@stud.ase.ro>
2021-05-19 14:43:25 +0300

    test JUnit

---
+++ Seminare/src/ro/ase/csie/cts/sem12/TestareUnitara/exceptii/ExceptieImpartireLaZero.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.sem12.TestareUnitara.exceptii;
+
+public class ExceptieImpartireLaZero extends Exception {
+
+}
---
+++ Seminare/src/ro/ase/csie/cts/sem12/TestareUnitara/modele/OperatiiMatematice.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.sem12.TestareUnitara.modele;
+
+import ro.ase.csie.cts.sem12.TestareUnitara.exceptii.ExceptieImpartireLaZero;
+
+public class OperatiiMatematice {
+
+	public static int aduna(int a, int b) {
+		return a + b;
+	}
+
+	public static double impartire(int a, int b) throws ExceptieImpartireLaZero {
+		if(b == 0) {
+			throw new ExceptieImpartireLaZero();
+		}
+		return a / b;
+	}
+}
